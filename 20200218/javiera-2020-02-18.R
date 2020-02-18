1e2e6f2a09871b563a5267c59245bcf59f0ff11c
Javi Rudolph <javiera.av@ufl.edu>
2020-02-18 13:15:50 -0500

    Figure!

---
+++ 20200218/javiera-2020-02-18.R
@@ -0,0 +1,20 @@
+
+#Read in the data
+
+food_consumption <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
+
+# libraries
+library(tidyverse)
+
+# Figure
+# How does CO2 relate to consumption?
+food_consumption %>%
+  ggplot(., aes(x = consumption, y = co2_emmission, color = food_category)) +
+  geom_point(size = 3, alpha = 0.6) +
+  theme_classic() +
+  guides(color = guide_legend(title = "Food")) +
+  labs(x = "Per capita consumption",
+       y = "CO2 emmission") +
+  theme(legend.position = "bottom")
+
+ggsave("20200218/tidytues-javi-2020-02-18.png")
