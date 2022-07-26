8f7edd42dba12c42051c412ae45d7e5fdc8261cf
JaviNano <f.javierarudolph@gmail.com>
2022-07-26 08:46:57 -0400

    tidy tuesday from a couple of weeks ago

---
+++ 2022/2022-07-12/README.md
@@ -0,0 +1,1 @@
+I'm working backwards but, the data for this one is [here](https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-07-12)
---
+++ 2022/2022-07-12/flights.R
@@ -0,0 +1,13 @@
+# TidyTuesday by javirudolph
+# data and info here: https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-07-12
+
+
+
+# Get the data
+flights <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-12/flights.csv')
+
+
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(stringr)
