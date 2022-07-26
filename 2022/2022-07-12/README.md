d3f7b0a91f8ebb83d1ffcaf89c28da9e0e4a03f2
JaviNano <f.javierarudolph@gmail.com>
2022-07-26 09:40:32 -0400

    done! Argh. trying to not do more than an hour on this.

--- 2022/2022-07-12/README.md
+++ 2022/2022-07-12/README.md
@@ -1,1 +1,3 @@
 I'm working backwards but, the data for this one is [here](https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-07-12)
+
+![]("flights.png")
--- 2022/2022-07-12/flights.R
+++ 2022/2022-07-12/flights.R
@@ -11,3 +11,29 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(stringr)
+
+
+# Top 10 countries with flights in 2021
+
+flights %>%
+  filter(., YEAR == 2021) %>%
+  group_by(STATE_NAME) %>%
+  summarise(total = sum(FLT_TOT_1)) %>%
+  arrange(desc(total)) %>%
+  slice_head(., n = 10) -> top10
+
+flights %>%
+  filter(., YEAR %in% c("2019", "2020", "2021") & STATE_NAME %in% top10$STATE_NAME) %>%
+  group_by(., YEAR, MONTH_NUM, STATE_NAME) %>%
+  summarise(T_FLT = sum(FLT_TOT_1)) %>%
+  ungroup() %>%
+  ggplot(., aes(x = MONTH_NUM, y = T_FLT, color = STATE_NAME, group = STATE_NAME)) +
+  facet_wrap(~YEAR, ncol = 1) +
+  # geom_point() +
+  geom_line(size = 1) +
+  theme_bw() +
+  labs(x = 'Month', y = 'Total flights') +
+  scale_color_viridis_d() +
+  theme(legend.title = element_blank())
+
+ggsave("2022/2022-07-12/flights.png", width = 6, height = 8, units = "in")
