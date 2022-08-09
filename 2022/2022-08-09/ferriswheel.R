d604ae42c17755a9afe19493cf0f65cea6eaee38
JaviNano <f.javierarudolph@gmail.com>
2022-08-09 10:00:22 -0400

    Working on plot 1 of my tidytuesday

---
+++ 2022/2022-08-09/ferriswheel.R
@@ -0,0 +1,66 @@
+# Get the Data
+
+# Or read in the data manually
+
+wheels <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-09/wheels.csv')
+
+
+# Explore the thing
+library(dplyr)
+library(tidyr)
+
+head(wheels)
+unique(wheels$country)
+
+# Look at the tallest wheels
+
+wheels %>%
+  arrange(., desc(height)) %>%
+  # and focus on the ones currently operating
+  filter(status == "Operating") %>%
+  drop_na(diameter, ride_duration_minutes)-> openwheels
+
+# What I want to explore:
+# Relationship between diameter and height which should be correlated
+# And I expect smaller diameter wheels to have shorter duration of rides
+
+library(ggplot2)
+# TOP 20
+openwheels %>%
+  slice_head(n=20) %>%
+  ggplot(., aes(y = diameter, x = height)) +
+  geom_point(aes(color = ride_duration_minutes)) +
+  geom_smooth(method = "lm")
+
+# Things to do:
+#   1. Change the theme
+#   2. write the formula in the figure
+#   3. move the legend inside the plotting area
+#   4. make points bigger and more obvious
+#   5. Change color
+
+mycols <- c("#003844", "#005256", "#006C67",
+            "#79808E", "#F194B4", "#F59C87",
+            "#F8A35A", "#FFB100", "#FFCE63")
+scales::show_col(mycols)
+
+# "#FFEBC6"
+library(ggpubr)
+
+openwheels %>%
+  slice_head(n=20) %>%
+  ggplot(., aes(y = diameter, x = height)) +
+  geom_smooth(method = "lm", color = "black") +
+  geom_point(aes(color = ride_duration_minutes), size = 4, alpha = 0.9) +
+  scale_color_gradient(low = mycols[3], high =  mycols[8]) +
+  guides(color = guide_colorbar(title = "Ride duration in minutes", direction = "horizontal",
+                                barwidth = 10, barheight = 0.5, title.position = "top")) +
+  labs(x = "Height in meters", y = "Diameter in meters") +
+  stat_cor(label.y = 490, aes(label =  ..rr.label..)) +
+  stat_regline_equation(label.y = 460) +
+  theme_minimal() +
+  theme(legend.position = c(0.8, 0.1),
+        legend.box.background = element_rect(color = "grey"))
+
+
+# Then, map where these top 20 wheels are located.
